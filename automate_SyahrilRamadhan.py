8f5861c8ac65cd227bfb722a642a17eb2c2dc46e
syahril ramadhan <syahril231072@gmail.com>
2026-05-31 15:27:31 +0000

    Fix MLflow CI issue

--- automate_SyahrilRamadhan.py
+++ automate_SyahrilRamadhan.py
@@ -125,10 +125,6 @@ X_test_scaled = scaler.transform(
 # MLFLOW CONFIG
 # ==========================================
 
-mlflow.set_tracking_uri(
-    "http://127.0.0.1:5000"
-)
-
 mlflow.set_experiment(
     EXPERIMENT_NAME
 )
